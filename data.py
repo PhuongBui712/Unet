30ce7348a61eceef03fbd824ab2451d0d06660ac
PhuongBui712 <Buitanphuong712@gmail.com>
2024-06-13 16:49:39 +0700

    add training, dataset code

---
+++ data.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class CarvanaDataset(Dataset):
+    SIZE = (512, 512)
+
+    def __init__(self, path, split):
+        if split == 'test':
+            # TODO: handle test set
+            pass
+        else:
+            data_dir = os.path.join(os.path.dirname(__file__), path)
+            self.images = [data_dir + '/train/' + file for file in os.listdir(data_dir + '/train/')]
+            self.masks = [data_dir + '/train_masks/' + file for file in os.listdir(data_dir + '/train_masks/')]
+
+        self.transformer = transforms.Compose([
+            transforms.Resize(size=self.SIZE),
+            transforms.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image = Image.open(self.images[index])
+        mask = Image.open(self.masks[index]).convert('L')
+
+        return self.transformer(image), self.transformer(mask)
